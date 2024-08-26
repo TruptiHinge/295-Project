04c051fb26db2add6681604f6af7f61848a85335
TruptiHinge <89198862+TruptiHinge@users.noreply.github.com>
2024-08-26 16:08:59 +0530

    Add files via upload

---
+++ PRO-C295-Project-Boilerplate-main/controllers/my_controller/my_controller.py
@@ -0,0 +1,24 @@
+from controller import Robot, Keyboard, Motion
+
+timestep=32
+
+robot= Robot()
+keyboard = Keyboard()
+keyboard.enable(timestep)
+
+wave = Motion('../../motions/wave.motion')
+
+
+def startMotion(motion):
+    motion.play()
+   
+
+key = -1
+
+
+while robot.step(timestep) != -1:
+
+    key = keyboard.getKey()  
+
+    if key == Keyboard.UP:
+        wave.play()
